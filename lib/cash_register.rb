32cad628c6f24836b8ff4289348ce8c7fa0df217
github username <github email address>
2019-06-26 17:05:07 -0400

    #total done, working on next methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
 
+    attr_accessor :total, :discount
+
+    def initialize(discount = 20)
+        @total = 0
+        @discount = discount
+    end
+
+    def add_item(title,price,quantity = 0)
+        original_total = @total
+        @total += price
+        original_total
+    end
+
+end
